ca1e3788ac2dc0dc8a6130bb0e32e6c1e1b6cf8e
Omar Ashraf <omarashrf15@gmail.com>
2021-10-03 20:02:23 +0200

    Updated for hard version with time limit for the quiz

--- quiz.go
+++ quiz.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type problem struct {
@@ -21,26 +22,45 @@ func parseLine(lines [][]string) []problem {
 	return ret
 }
 
+func getAnswer(ch chan string) {
+	var text string
+	fmt.Scanf("%s\n", &text)
+	ch <- text
+}
+
 func main() {
 	fptr := flag.String("fpath", "problems.csv", "file path to read from")
+	timeLimit := flag.Int("limit", 1, "Time limit for the quiz")
 	flag.Parse()
+
 	f, err := os.Open(*fptr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	r := csv.NewReader(f)
+
 	cnt := 0
 	lines, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	problems := parseLine(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	for idx, problem := range problems {
 		fmt.Printf("Problem #%d: %s = \n", idx+1, problem.question)
-		var text string
-		fmt.Scanf("%s\n", &text)
-		if text == problem.answer {
-			cnt = cnt + 1
+		inputAnswer := make(chan string)
+		go getAnswer(inputAnswer)
+
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %d from 12", cnt)
+			return
+		case text := <-inputAnswer:
+			if text == problem.answer {
+				cnt++
+			}
 		}
 	}
 	fmt.Printf("You scored %d from 12", cnt)
